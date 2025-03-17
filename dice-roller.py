df530cb83f4978ad894c8392cdef127a43846f69
the-destructor <73207042+the-destructor@users.noreply.github.com>
2025-03-18 09:21:07 +1100

    update dice roller

--- dice-roller.py
+++ dice-roller.py
@@ -7,26 +7,28 @@
 - Description: Complete a functional dice roller app in Python
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 import random
-def convert(number):
-    try:
-        int(number)
-        return(int(number))
-    except ValueError:
-         print("Value ERRORRR!!!")
+
+def get_number(type):
+    while True:
+        bob = input(f"Enter a number for the {type}: ")
+        try:
+            return(int(bob))
+        except ValueError:
+            print("")
+
 
 def dice_roll():
     total = 0
-    for i in range(convert(Count)):
-        rollval = random.randint(1, convert(Type))
+    for i in range(1, Count):
+        rollval = random.randint(1, Type)
         print(f"Spin {i+1}: {rollval}")
         total = total + rollval
 
     print(f"Total of dice rolls == {total}")
 
 while True:
-    Type = input("Side count of dice: ")
-    Count = input("Amount of dice rolled: ")
-
+    Type = get_number("side count of dice")
+    Count = get_number("amount of dice rolled")
     dice_roll()
     bob = input("Do you want to run again? Y/N: ")
     if bob != "Y" and bob != "y":
